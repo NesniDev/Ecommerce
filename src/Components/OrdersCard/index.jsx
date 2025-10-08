3d577478c63c24d30929b4f687bdb1ba6918ef64
Neider Nieto <nestnidev17@gmail.com>
2025-10-08 17:20:01 -0500

    styles for OrdersCard

--- src/Components/OrdersCard/index.jsx
+++ src/Components/OrdersCard/index.jsx
@@ -1,15 +1,22 @@
-import { XMarkIcon } from '@heroicons/react/16/solid'
+import { ChevronRightIcon } from '@heroicons/react/16/solid'
 
-const OrderCard = ({ totalPrice, totalProducts }) => {
+const OrdersCard = ({ totalPrice, totalProducts }) => {
   return (
-    <article className="flex justify-between items-center px-3 mb-2">
-      <div className="flex items-center gap-3">
-        <span>{new Date().toDateString()}</span>
-        <span>{totalProducts}</span>
-        <span>{totalPrice}</span>
+    <article className="flex justify-between items-center px-3 mb-2 gap-3 p-4 my-2 border border-black rounded-lg">
+      <div className="flex justify-between items-center w-60">
+        <p className="flex flex-col">
+          <span className="text-xs font-light">
+            {new Date().toISOString().split('T')[0]}
+          </span>
+          <span className="text-xs font-semibold">{totalProducts} article</span>
+        </p>
+        <p className="flex items-center gap-2">
+          <span className="text-xl font-semibold">$ {totalPrice}</span>
+          <ChevronRightIcon className="w-6 h-6 cursor-pointer" />
+        </p>
       </div>
     </article>
   )
 }
 
-export default OrderCard
+export default OrdersCard
