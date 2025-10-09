0645ee3b7fd1a661b045e9b73dc6fe16349066f8
Neider Nieto <nestnidev17@gmail.com>
2025-10-08 23:27:32 -0500

    refactor  Home

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,26 +1,40 @@
-import { useState, useEffect } from 'react'
 import Card from '../../Components/Card'
 import Layout from '../../Components/Layout'
 import '../../index.css'
 import ProductDetail from '../../Components/ProductDetail'
+import { useContext } from 'react'
+import { ShoppingCartContext } from '../../Context/ShoppingCartContext'
 
 function Home() {
-  const [items, setItems] = useState([])
-  useEffect(() => {
-    fetch('https://api.escuelajs.co/api/v1/products')
-      .then((res) => res.json())
-      .then((data) => {
-        setItems(data)
+  const context = useContext(ShoppingCartContext)
+
+  const renderView = () => {
+    if (context.filteredItems?.length > 0) {
+      return context.filteredItems?.map((item) => {
+        return <Card key={item.id} {...item} />
       })
-      .catch((error) => console.error('Error fetching products:', error))
-  }, [])
+    } else {
+      return (
+        <div>
+          <h2>No products found</h2>
+        </div>
+      )
+    }
+  }
+
   return (
     <Layout>
-      <h1 className="text-3xl font-bold underline">Home</h1>
+      <div className="flex items-center justify-between">
+        <h1 className="text-xl font-medium">Exclusive Products</h1>
+      </div>
+      <input
+        type="text"
+        placeholder="Search"
+        className="border border-gray-950 rounded-lg px-3 py-2 mt-4 focus:outline-none"
+        onChange={(e) => context.setSearchByTitle(e.target.value)}
+      />
       <section className="mt-6 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {items?.map((item) => {
-          return <Card key={item.id} {...item} />
-        })}
+        {renderView()}
         <ProductDetail />
       </section>
     </Layout>
